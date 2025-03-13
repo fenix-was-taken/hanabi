76a9109b588db9b848f9837ca4d9c1660a8f0ce9
jrey11 <jared.reyes@coyotes.usd.edu>
2025-03-13 18:21:42 -0500

    Initial commit...

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <genesis.h>
+#include <resources.h>
+#include <string.h>
+
+int main()
+{
+    VDP_drawText("Hello World!", 10,13);
+    while(1)
+    {
+        //For versions prior to SGDK 1.60 use VDP_waitVSync instead.
+        SYS_doVBlankProcess();
+    }
+    return (0);
+}
